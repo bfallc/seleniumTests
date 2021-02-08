bed68b4d950af5a9288469ac2fe005c792c40dc7
bfallc <bfallc@artic.edu>
2021-02-07 21:50:08 -0500

    data fill

--- scripts/2021-02-03_testing.py
+++ scripts/2021-02-03_testing.py
@@ -15,7 +15,7 @@ from selenium.webdriver.common.keys import Keys
 # "which geckodriver" into term to find path
 driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
 
-driver.get("https://bfallc.github.io/seleniumTests/testPages/")
+# driver.get("https://bfallc.github.io/seleniumTests/testPages/")
 # elem2 = driver.find_element_by_css_selector('a[href*="statement"]')
 # driver.execute_script("arguments[0].scrollIntoView();", elem2)
 
@@ -23,66 +23,113 @@ driver.get("https://bfallc.github.io/seleniumTests/testPages/")
 # actions.move_to_element(elem2).click().perform()
 
 # #open a website
-# driver.get("https://www.amazon.com")
+driver.get("https://www.amazon.com")
 
 # #check that "python" is in the webpage title
-# assert "Amazon" in driver.title
+assert "Amazon" in driver.title
 
 # #find the searchbar (named "q")
-# elem = driver.find_element_by_name("field-keywords")
-# #clear the text box just in case
-# elem.clear()
-# #type some stuff
-# elem.send_keys("house")
-# #hit return
-# elem.send_keys(Keys.RETURN)
+elem = driver.find_element_by_name("field-keywords")
 
-# time.sleep(5)
+#scroll into view (just in case)
+driver.execute_script("arguments[0].scrollIntoView();", elem)
+
+# #clear the text box just in case
+elem.clear()
+
+# #type some stuff
+elem.send_keys("house")
+
+#optional
+time.sleep(3)
+
+#hit return
+elem.send_keys(Keys.RETURN)
+
+time.sleep(5)
 # print("waited 3 sec")
 
 #find the second link in search results
 
-# #doesn't work with multiple items with same partial link
-# elem2 = driver.find_element_by_css_selector('a[href*="sr=8-2"]')
+# this finds the first element with data-index='3' (searching down from root), 
+# and then the h2/a element inside it
+elem2 = driver.find_element_by_xpath("//*[contains (@data-index, '3')]//h2/a")
 
-# #works
-# elem2 = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/span[3]/div[2]/div[3]/div/span/div/div/div[2]/div[2]/div/div[1]/h2/a")
+#prints the webdriver session id
+print("elem2: " + str(elem2))
+#prints the text node from inside elem2 (if there is one)
+print(elem2.text)
+
+# #scrolling. uses javascript due to a firefox quirk with webdriver
+driver.execute_script("arguments[0].scrollIntoView();", elem2)
+print("scrolled to element")
+
+#optional
+time.sleep(3)
+
+#must be in view to click
+elem2.click()
+print("clicked") 
+
+#not sure
+# driver.implicitly_wait(3)
+# print("waited 3 sec if necessary")
+
+#optional
+time.sleep(5)
+
+#use javascript to go back one page in history (back button)
+driver.execute_script("window.history.go(-1)")
+
+time.sleep(3)
+
+#find next
+elem3 = driver.find_element_by_xpath("//*[contains (@data-index, '5')]//h2/a")
+
+#scroll
+driver.execute_script("arguments[0].scrollIntoView();", elem3)
+
+#optional
+time.sleep(3)
+
+#click
+elem3.click()
+
+time.sleep(5)
+
+#go back
+driver.execute_script("window.history.go(-1)")
+
+time.sleep(5)
+
+#go back
+driver.execute_script("window.history.go(-1)")
+
+## multi-element lists below
+##########################
 
 # searchResults = driver.find_elements_by_partial_link_text("result")
-searchResults = driver.find_elements_by_xpath(".//span")
-# searchR = driver.find_elements(By.)
-print("elem2")
-for r in searchResults:
-    print(r.text)
+# searchResults = driver.find_elements_by_xpath("//span[@class='linktext' and text()='outside']")
 
-# #scrolling (works)
-# driver.execute_script("arguments[0].scrollIntoView();", elem2)
-# print("scrolled to element")
+# for r in searchResults:
+#     print(r.text)
 
-# elem2.click()
-# print("clicked") 
+
+
+## ActionChains
+####################
 
 # #not working yet:
 # actions = ActionChains(driver)
 # actions.move_to_element(elem2).click().perform()
 # print("clicked once")
 
-driver.implicitly_wait(3)
-print("waited 3 sec")
-#click link with 'sr=8-2' in address (second link)
-# driver.find_element_by_css_selector('a[href*="sr=8-1"]').click()
-
-
-
-
-#if assert is true, nothing happens, otherwise assertion error occurs
-#if "no results found" is contained in the page source, throw the error "nothing found"
-#assert "No results found." not in driver.page_source, "nothing found"
 
 #alternatively, use an if statement:
-if "No results found." not in driver.page_source:
-    print("results found")
+# if "No results found." not in driver.page_source:
+#     print("results found")
 
 #close the browser window
 # driver.quit()
 
+time.sleep(4)
